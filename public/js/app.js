1ef1e213a5c25ed6ec928c216c1127894d116496
akashgupta-262 <akash.gupta@creatiosoft.com>
2021-09-29 13:15:17 +0530

    akash

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,24 @@
-const { response } = require("express")
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
-console.log("hello sky")
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-fetch('https://puzzle.mead.io/puzzle').then((response)=>{
-    response.json().then((data)=>{
-        console.log(data)
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
     })
 })
